62ed551806536f6f10f097bd03a253b450a4e368
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-05-10 16:27:14 +0200

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+console.log('wos is do los?');
+
+let num = 5;
+let anotherNum = 8;
+
+
+console.log(num == anotherNum);
+console.log(num > anotherNum);
+console.log(num < anotherNum);
+console.log(num >= anotherNum);
+console.log(num <= anotherNum);
+
+
+console.log(num == "5");
+console.log(num != 8);
+console.log(num != "5");
